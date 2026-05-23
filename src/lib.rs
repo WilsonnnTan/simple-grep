5669801e40c47a9e15e50f8ea6a577d0766fb0ec
WilsonnnTan <wilsonangelietann@gmail.com>
2026-05-23 23:51:47 +0700

    feat: add case insensitive search with unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 pub fn search <'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+	let query = query.to_lowercase();
 	let mut results: Vec<&str> = Vec::new();
 
 	for line in contents.lines() {
-		if line.contains(query) {
+		if line.to_lowercase().contains(&query) {
 			results.push(line);
 		}
 	}
@@ -23,5 +24,17 @@ safe, fast, productive.
 Pick three.";
 
 		assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+	}
+
+	#[test]
+	fn case_insensitive() {
+		let query = "rUst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+		assert_eq!(vec!["Rust:", "Trust me."], search(query, contents));
 	}
 }
